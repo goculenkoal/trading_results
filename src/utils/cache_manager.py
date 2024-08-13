181e9f33f69e4c91a7b6fb888bea7104f82816d4
alex <goculenko.al@gmail.com>
2024-08-13 23:55:32 +0300

    refactor, add cashe_clear to cashe_manager

--- src/utils/cache_manager.py
+++ src/utils/cache_manager.py
@@ -1,6 +1,3 @@
-import asyncio
-from datetime import datetime
-
 from fastapi_cache.backends.redis import RedisBackend
 from redis import asyncio as aioredis
 from fastapi_cache import FastAPICache
@@ -16,19 +13,13 @@ class CacheManager:
         print("Подключился к редис")
         FastAPICache.init(RedisBackend(self.redis), prefix="fastapi-cache")
 
-        asyncio.create_task(self.reset_cache())
+    async def clear_cache(self):
+        try:
+            await self.redis.flushdb()  # Очищаем базу данных Redis
+            print("Cache cleared")  # Можно использовать логирование
+        except Exception as e:
+            print(f"Error clearing cache: {e}")
 
     async def close(self):
         if self.redis:
             await self.redis.close()
-
-    async def reset_cache(self):
-        while True:
-            now = datetime.now()
-            # Сбрасываем кэш в 14:11
-            if now.hour == 14 and now.minute == 11:
-                print("Drop Cache")
-                await FastAPICache.clear()
-                await asyncio.sleep(60)
-            else:
-                await asyncio.sleep(60)  # Ожидание 1 минуты перед следующей проверкой
--- src/utils/main.py
+++ src/utils/main.py
@@ -1,6 +1,9 @@
 import os
 from contextlib import asynccontextmanager
 
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
+from apscheduler.triggers.cron import CronTrigger
+
 from dotenv import load_dotenv, find_dotenv
 from fastapi import FastAPI
 from fastapi.responses import ORJSONResponse
@@ -10,13 +13,31 @@ from src.utils.metadata import TITLE, DESCRIPTION, VERSION, TAG_METADATA
 from src.utils.config import settings
 from src.utils.cache_manager import CacheManager
 
+scheduler = AsyncIOScheduler()
+
 
 @asynccontextmanager
 async def lifespan(_app: FastAPI):
     cache_manager = CacheManager(settings.REDIS_URL)
     await cache_manager.initialize()  # Инициализация кэширования
+
+    # Настройка задачи для очистки кэша
+    async def clean_cache():
+        await cache_manager.clear_cache()  # Вызываем метод очистки кэша
+
+    # Настройка триггера Cron для выполнения задачи в 14:11 каждый день
+    scheduler.add_job(
+        func=clean_cache,  # Указываем асинхронную функцию для очистки кэша
+        trigger=CronTrigger(hour=23, minute=53),  # Время для очистки кэша
+        id='cache_cleanup_job',
+        replace_existing=True
+    )
+
+    scheduler.start()
+
     yield
     await cache_manager.close()  # Закрытие соединения при завершении
+    scheduler.shutdown()
 
 
 def create_fast_api_app():
@@ -52,3 +73,4 @@ def create_fast_api_app():
 
 
 app = create_fast_api_app()
+

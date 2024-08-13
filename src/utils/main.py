03d9b6a79791d25f26e717ae59c8c9f042d17516
alex <goculenko.al@gmail.com>
2024-08-13 15:14:53 +0300

    modified main

--- src/utils/main.py
+++ src/utils/main.py
@@ -1,15 +1,43 @@
+import os
+
+from dotenv import load_dotenv, find_dotenv
 from fastapi import FastAPI
+from fastapi.responses import ORJSONResponse
 
 from src.api.routers.trade.trade import router
+from src.utils.metadata import TITLE, DESCRIPTION, VERSION, TAG_METADATA
 
-app = FastAPI(
-    title='Trading Results'
-)
+
+def create_fast_api_app():
+    load_dotenv(find_dotenv(".env"))
+    env_name = os.getenv('MODE', 'DEV')
+
+    if env_name != 'PROD':
+        _app = FastAPI(
+            default_response_class=ORJSONResponse,
+            title=TITLE,
+            description=DESCRIPTION,
+            version=VERSION,
+            openapi_tags=TAG_METADATA
+        )
+    else:
+        _app = FastAPI(
+            default_response_class=ORJSONResponse,
+            title=TITLE,
+            description=DESCRIPTION,
+            version=VERSION,
+            openapi_tags=TAG_METADATA,
+            docs_url=None,
+            redoc_url=None
+        )
+
+    _app.include_router(router, prefix='/api')
+    return _app
+
+
+app = create_fast_api_app()
 
 
 @app.get('/')
 def hello():
-    return "Hello world"
-
-
-app.include_router(router)
+    return "Welcome"

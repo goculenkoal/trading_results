c06a229ec34880e80301edbca34615fde596401e
alex <goculenko.al@gmail.com>
2024-08-20 17:40:43 +0300

    repository refactor

--- src/repositories/spimex.py
+++ src/repositories/spimex.py
@@ -1,3 +1,9 @@
+from datetime import datetime
+from typing import Sequence
+
+from sqlalchemy import select
+
+from src.schemas.schemas import TradingResultDateSchema
 from src.models.trade import SpimexTradingResults
 from src.utils.repository import SqlAlchemyRepository
 
@@ -6,3 +12,49 @@ class SpimexRepository(SqlAlchemyRepository):
     """класс для работы репозитория чз модель."""
 
     model = SpimexTradingResults
+
+    async def get_by_query_all_limit(self, limit: int) -> Sequence[type(model)]:
+        query = select(self.model).distinct().limit(limit=limit)
+        result = await self.session.execute(query)
+        return result.scalars().all()
+
+    async def get_by_query_dates(self, limit: int) -> Sequence[type(model)]:
+        query = (select(self.model.date)
+                 .distinct().order_by(self.model.date.desc())
+                 .limit(limit=limit))
+        result = await self.session.execute(query)
+        trades = result.scalars().all()
+
+        return [TradingResultDateSchema(date=data) for data in trades]
+
+    async def get_by_query_all(
+            self,
+            oil_id: str,
+            delivery_type_id: str,
+            delivery_basis_id: str,
+    ) -> Sequence[type(model)]:
+        query = (select(self.model)
+                 .filter(self.model.oil_id == oil_id)
+                 .filter(self.model.delivery_type_id == delivery_type_id)
+                 .filter(self.model.delivery_basis_id == delivery_basis_id)
+                 )
+        result = await self.session.execute(query)
+        return result.scalars().all()
+
+    async def get_by_query_all_dynamics(
+            self,
+            oil_id: str,
+            delivery_type_id: str,
+            delivery_basis_id: str,
+            start_date: datetime,
+            end_date: datetime,
+    ) -> Sequence[type(model)]:
+        query = (select(self.model)
+                 .filter(self.model.oil_id == oil_id)
+                 .filter(self.model.delivery_type_id == delivery_type_id)
+                 .filter(self.model.delivery_basis_id == delivery_basis_id)
+                 .filter(self.model.date >= start_date)
+                 .filter(self.model.date <= end_date)
+                 ).limit(30)
+        result = await self.session.execute(query)
+        return result.scalars().all()
--- src/utils/repository.py
+++ src/utils/repository.py
@@ -1,69 +1,90 @@
 from abc import ABC, abstractmethod
 from collections.abc import Sequence
-from datetime import datetime
-from typing import Any, Never
+from typing import Union
 
-from sqlalchemy import select
+from sqlalchemy import select, insert, Result, update, delete
 from sqlalchemy.ext.asyncio import AsyncSession
-
-from src.schemas.schemas import TradingResultDateSchema
 
 
 class AbstractRepository(ABC):
+    class AbstractRepository(ABC):
+        @abstractmethod
+        async def add_one(self, *args, **kwargs):
+            raise NotImplementedError
 
-    @abstractmethod
-    async def get_by_query_all_limit(self, *args: Any, **kwargs: Any) -> Never:
-        raise NotImplementedError
+        @abstractmethod
+        async def add_one_and_get_id(self, *args, **kwargs):
+            raise NotImplementedError
+
+        async def add_one_and_get_obj(self, *args, **kwargs):
+            raise NotImplementedError
+
+        async def get_by_query_one_or_none(self, *args, **kwargs):
+            raise NotImplementedError
+
+        @abstractmethod
+        async def get_by_query_all(self, *args, **kwargs):
+            raise NotImplementedError
+
+        @abstractmethod
+        async def update_one_by_id(self, *args, **kwargs):
+            raise NotImplementedError
+
+        @abstractmethod
+        async def delete_by_query(self, *args, **kwargs):
+            raise NotImplementedError
+
+        @abstractmethod
+        async def delete_all(self, *args, **kwargs):
+            raise NotImplementedError
 
 
 class SqlAlchemyRepository(AbstractRepository):
+    """
+    A basic repository that implements basic CRUD functions with a base table using the SqlAlchemy library
+
+    params:
+        - model: SQLAlchemy DeclarativeBase child class
+    """
+
     model = None
 
-    def __init__(self, session: AsyncSession) -> None:
+    def __init__(self, session: AsyncSession):
         self.session = session
 
-    async def get_by_query_all_limit(self, limit: int) -> Sequence[type(model)]:
-        query = select(self.model).distinct().limit(limit=limit)
-        result = await self.session.execute(query)
-        return result.scalars().all()
+    async def add_one(self, **kwargs) -> None:
+        query = insert(self.model).values(**kwargs)
+        await self.session.execute(query)
 
-    async def get_by_query_dates(self, limit: int) -> Sequence[type(model)]:
-        query = (select(self.model.date)
-                 .distinct().order_by(self.model.date.desc())
-                 .limit(limit=limit))
-        result = await self.session.execute(query)
-        trades = result.scalars().all()
+    async def add_one_and_get_id(self, **kwargs) -> Union[int, str]:
+        query = insert(self.model).values(**kwargs).returning(self.model.id)
+        _id: Result = await self.session.execute(query)
+        return _id.scalar_one()
 
-        return [TradingResultDateSchema(date=data) for data in trades]
+    async def add_one_and_get_obj(self, **kwargs) -> type(model):
+        query = insert(self.model).values(**kwargs).returning(self.model)
+        _obj: Result = await self.session.execute(query)
+        return _obj.scalar_one()
 
-    async def get_by_query_all(
-            self,
-            oil_id: str,
-            delivery_type_id: str,
-            delivery_basis_id: str,
-    ) -> Sequence[type(model)]:
-        query = (select(self.model)
-                 .filter(self.model.oil_id == oil_id)
-                 .filter(self.model.delivery_type_id == delivery_type_id)
-                 .filter(self.model.delivery_basis_id == delivery_basis_id)
-                 )
-        result = await self.session.execute(query)
-        return result.scalars().all()
+    async def get_by_query_one_or_none(self, **kwargs) -> type(model) | None:
+        query = select(self.model).filter_by(**kwargs)
+        res: Result = await self.session.execute(query)
+        return res.unique().scalar_one_or_none()
 
-    async def get_by_query_all_dynamics(
-            self,
-            oil_id: str,
-            delivery_type_id: str,
-            delivery_basis_id: str,
-            start_date: datetime,
-            end_date: datetime,
-    ) -> Sequence[type(model)]:
-        query = (select(self.model)
-                 .filter(self.model.oil_id == oil_id)
-                 .filter(self.model.delivery_type_id == delivery_type_id)
-                 .filter(self.model.delivery_basis_id == delivery_basis_id)
-                 .filter(self.model.date >= start_date)
-                 .filter(self.model.date <= end_date)
-                 ).limit(30)
-        result = await self.session.execute(query)
-        return result.scalars().all()
+    async def get_by_query_all(self, **kwargs) -> Sequence[type(model)]:
+        query = select(self.model).filter_by(**kwargs)
+        res: Result = await self.session.execute(query)
+        return res.scalars().all()
+
+    async def update_one_by_id(self, _id: Union[int, str], values: dict) -> type(model) | None:
+        query = update(self.model).filter(self.model.id == _id).values(**values).returning(self.model)
+        _obj: Result | None = await self.session.execute(query)
+        return _obj.scalar_one_or_none()
+
+    async def delete_by_query(self, **kwargs) -> None:
+        query = delete(self.model).filter_by(**kwargs)
+        await self.session.execute(query)
+
+    async def delete_all(self) -> None:
+        query = delete(self.model)
+        await self.session.execute(query)
